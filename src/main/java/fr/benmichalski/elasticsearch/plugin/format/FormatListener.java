eb5769770ec227cda4127ad2d27fcc3ff8664aa4
Benjamin Michalski <benjamin.michalski@gmail.com>
2015-07-10 00:15:55 +0200

    Dev.

--- src/main/java/fr/benmichalski/elasticsearch/plugin/format/FormatListener.java
+++ src/main/java/fr/benmichalski/elasticsearch/plugin/format/FormatListener.java
@@ -1,12 +1,26 @@
 package fr.benmichalski.elasticsearch.plugin.format;
 
+import au.com.bytecode.opencsv.CSVWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Map;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.rest.BytesRestResponse;
 import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestResponse;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.rest.action.support.RestResponseListener;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHitField;
 
 public class FormatListener extends RestResponseListener<SearchResponse> {
+
+    private final static ESLogger logger = ESLoggerFactory.getLogger("format");
 
     public FormatListener(RestChannel channel) {
         super(channel);
@@ -14,10 +28,60 @@ public class FormatListener extends RestResponseListener<SearchResponse> {
 
     @Override
     public final RestResponse buildResponse(SearchResponse response) throws Exception {
-        return new BytesRestResponse(
-            response.status(),
-            "text/plain",
-            "foobar"
-        );
+        try {
+            XContentBuilder builder = this.channel.newBuilder();
+
+            OutputStream stream = builder.stream();
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(stream);
+
+            CSVWriter csvWriter = new CSVWriter(outputStreamWriter);
+
+            int i = 0;
+
+            logger.info("Total hits: " + (new Long(response.getHits().totalHits())).toString());
+
+            for (SearchHit hit : response.getHits().getHits()) {
+                ++i;
+
+                Map<String, Object> sourceAsMap = hit.sourceAsMap();
+
+                ArrayList<String> stringList = new ArrayList<String>(sourceAsMap.size());
+
+                for (Map.Entry<String, Object> field : sourceAsMap.entrySet()) {
+                    String key = field.getKey();
+
+                    stringList.add(XContentMapValues.extractValue(key, sourceAsMap).toString());
+                }
+
+                String[] stringArr = new String[stringList.size()];
+                stringArr = stringList.toArray(stringArr);
+
+                csvWriter.writeNext(stringArr);
+
+                if (i % 1000 == 0) {
+                    csvWriter.flush();
+                }
+            }
+
+            //Final flush
+            csvWriter.flush();
+
+            BytesRestResponse bytesRestResponse = new BytesRestResponse(
+                response.status(),
+                builder
+            );
+
+            String scrollId = response.getScrollId();
+
+            if (null != scrollId) {
+                bytesRestResponse.addHeader("X-SCROLL-ID", response.getScrollId());
+            }
+
+            return bytesRestResponse;
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+            return new BytesRestResponse(channel, RestStatus.INTERNAL_SERVER_ERROR, t);
+        }
     }
 }
